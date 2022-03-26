572332e72cd952eec38ecf9267bf06612eb58467
Georgy <Egor-Nikitin02@yandex.ru>
2022-03-26 15:21:49 +0300

    Added the parser in the main method

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace ConsoleApplication
+﻿using System.Threading.Tasks;
+using Classes;
+
+namespace ConsoleApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            ToysParser toyParser = new ToysParser();
+            var toys = await toyParser.Parse("https://www.toy.ru/catalog/boy_transport/");
         }
     }
 }
