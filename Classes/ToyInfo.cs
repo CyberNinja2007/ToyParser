9858ccb9db86e27e63928681b510a378a44b0b88
Georgy <Egor-Nikitin02@yandex.ru>
2022-03-26 15:20:16 +0300

    Added class with the necessary information of toys

---
+++ Classes/ToyInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Classes
+{
+    public struct ToyInfo
+    {
+        public string Region { get; set; }
+        public string[] Breadcrumbs { get; set; }
+        public string Name { get; set; }
+        public string PriceNew { get; set; }
+        public string PriceOld { get; set; }
+        public string Availability { get; set; }
+        public string[] Pictures { get; set; }
+        public string Url { get; set; }
+    }
+}
