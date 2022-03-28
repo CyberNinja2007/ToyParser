fca3b06a94875994ddda20e8ceb2237da20e8471
Georgy <Egor-Nikitin02@yandex.ru>
2022-03-28 20:11:03 +0300

    Added new extension for the ToyInfo class

---
+++ Classes/ToysInfoToCsvExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Classes
+{
+    public static class ToysInfoToCsvExtension
+    {
+        public static string ToCsv(this ToyInfo toy)
+        {
+            StringBuilder result = new StringBuilder(toy.Url + ';' + toy.Region + ';');
+            
+            foreach (var breadcrumb in toy.Breadcrumbs)
+            {
+                result.Append(breadcrumb + ' ');
+            }
+
+            result.Append(';' + toy.Name + ';');
+
+            foreach (var picture in toy.Pictures)
+            {
+                result.Append(picture + ' ');
+            }
+
+            result.Append(';' + toy.PriceOld + ';' + toy.PriceNew + ';' + toy.Availability);
+
+            return result.ToString();
+        }
+    }
+}
