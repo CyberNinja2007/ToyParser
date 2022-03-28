96c1cb6e117aa20276fff40f278fafe1f13fbf40
Georgy <Egor-Nikitin02@yandex.ru>
2022-03-28 20:10:48 +0300

    Added writer to the csv file

---
+++ Classes/WriterCsv.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Classes
+{
+    public static class WriterCsv
+    {
+        public static void  Write(IEnumerable<String> data)
+        {
+            string filePath = @"Result.csv";
+            File.WriteAllLines(filePath, data);
+        }
+    }
+}
